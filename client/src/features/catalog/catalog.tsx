a1f7c784f37a022bce06642d1b67824db6d328e2
David Lima <David.lima17@gmail.com>
2022-11-05 16:25:54 -0500

    added error handling

--- client/src/features/catalog/catalog.tsx
+++ client/src/features/catalog/catalog.tsx
@@ -1,17 +1,22 @@
 import { Fragment, useEffect, useState } from "react";
+import agent from "../../api/agents";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 import { Product } from "../../app/models/product";
 import ProductList from "./productList";
 
 
 export default function Catalog(){
     const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:5000/api/products')
-        .then(response => response.json())
-        .then(data => setProducts(data))
+        agent.Catalog.list()
+            .then(products => setProducts(products))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false))
     }, [])
 
+    if (loading) return <LoadingComponent message="Loading Products..."/>
     return (
         <Fragment>
             <ProductList products={products} />
--- client/src/features/catalog/productDetails.tsx
+++ client/src/features/catalog/productDetails.tsx
@@ -1,7 +1,9 @@
 import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import agent from "../../api/agents";
+import NotFound from "../../app/errors/NotFound";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 import { Product } from "../../app/models/product";
 
 export default function ProductDetailsPage() {
@@ -10,15 +12,15 @@ export default function ProductDetailsPage() {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        axios.get(`http://localhost:5000/api/products/${id}`)
-            .then(response => setProduct(response.data))
+        agent.Catalog.details(parseInt(id))
+            .then(response => setProduct(response))
             .catch(error => console.log(error))
             .finally(() => setLoading(false));
     }, [id])
     
-    if(loading) return <h3>Loading...</h3>
+    if(loading) return <LoadingComponent message="Loading Product..."/>
 
-    if(!product) return <h3>Product not found</h3>
+    if(!product) return <NotFound />
 
 
     return(
