4610811dcc1a082df8ce443f8d038c0a05bfe896
David Lima <David.lima17@gmail.com>
2023-01-02 17:18:47 -0600

    implemented pagination options for the API side

---
+++ API/RequestHelpers/PaginationParams.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.RequestHelpers
+{
+    public class PaginationParams
+    {
+        private const int MAX_PAGE_SIZE = 50;
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = 6;
+        public int PageSize {
+            get => _pageSize;
+            set => _pageSize = value > MAX_PAGE_SIZE ? MAX_PAGE_SIZE : value;
+        }
+    }
+}
